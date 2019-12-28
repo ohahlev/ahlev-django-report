9168074c1cfff1902d4894b5b9018388524df4db
ahlev <ohahlev@gmail.com>
2019-12-28 22:24:19 +0700

    add files

--- report/admin.py
+++ report/admin.py
@@ -1,3 +1,76 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import ReportType, Report, ReportItem, ItemType
+import pprint
 
-# Register your models here.
+class ReportTypeAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'detail']
+    list_display = ['name', 'date_created', 'last_updated', 'preview_detail']
+    fieldsets = [
+        (None, {
+            'fields': ['name', 'detail'],
+        }),
+    ]
+
+    def preview_detail(self, obj):
+        return format_html(obj.detail)
+
+admin.site.register(ReportType, ReportTypeAdmin)
+
+class ReportItemInline(admin.StackedInline):
+    model = ReportItem
+    extra = 1
+
+class ReportAdmin(admin.ModelAdmin):
+    inlines = [ReportItemInline]
+    search_fields = ['name', 'date_from', 'date_to', 'detail']
+    list_display = ['name', 'date_created', 'last_updated', 'date_from', 'date_to', 'preview_detail']
+    
+    fieldsets = [
+        ('DATE RANGE', {
+            'fields': ['date_from', 'date_to'],
+        }),
+        ('REPORT DETAIL', {
+            'fields': ['report_type', 'name', 'detail'],
+        }),
+        (
+         'REPORT PREVIEW', {
+             'fields': ['preview_report']
+        }),
+    ]
+    readonly_fields = ['preview_report']
+
+    def preview_detail(self, obj):
+        return format_html(obj.detail)
+
+    preview_detail.short_description = 'detail'
+    preview_detail.admin_order_field = 'detail'
+
+    def preview_report(self, obj):
+        return format_html('''
+            {}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+            </table>
+        '''.format((obj.report_item.all())))
+
+    preview_report.short_description = 'Preview Report'
+
+admin.site.register(Report, ReportAdmin)
+
+class ItemTypeAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'detail']
+    list_display = ['name', 'date_created', 'last_updated', 'preview_detail']
+
+    def preview_detail(self, obj):
+        return format_html(obj.detail)
+
+    preview_detail.short_description = 'detail'
+    preview_detail.admin_order_field = 'detail'
+
+admin.site.register(ItemType, ItemTypeAdmin)
--- report/models.py
+++ report/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from tinymce.models import HTMLField
 
-# Create your models here.
+class ReportType(models.Model):
+    name = models.CharField(max_length=32, unique=True, blank=False)
+    detail = HTMLField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Report Type'
+        verbose_name_plural = 'Report Types'
+
+    def __str__(self):
+        return self.name
+
+class Report(models.Model):
+    name = models.CharField(max_length=32, unique=True, blank=False)
+    date_from = models.DateTimeField(verbose_name='from')
+    date_to = models.DateTimeField(verbose_name='to')
+    detail = HTMLField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    report_type = models.ForeignKey(ReportType, on_delete=models.CASCADE, verbose_name='Report Type')
+
+    class Meta:
+        verbose_name = 'Report'
+
+    def __str__(self):
+        return self.name
+
+class ItemType(models.Model):
+    name = models.CharField(max_length=32, unique=True, blank=False)
+    detail = HTMLField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Item Type'
+    
+    def __str__(self):
+        return self.name
+
+class ReportItem(models.Model):
+    name = models.CharField(max_length=32, unique=True, blank=True)
+    value = models.FloatField(blank=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE, verbose_name='Item Type')
+    report = models.ForeignKey(Report, on_delete=models.CASCADE, verbose_name='Report')
+
+    class Meta:
+        verbose_name = 'Report Item'
+        verbose_name_plural = 'Report Items'
+
+    def __str__(self):
+        return self.name
+
+
+
